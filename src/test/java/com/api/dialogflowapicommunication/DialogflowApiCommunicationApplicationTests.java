ca8a54f47b5dccca61461c7894ba766779504427
Raul Aquino <aquinoraul207@gmail.com>
2022-09-25 00:02:56 -0300

    start of a spring boot project to try to connect with the dialogflow API

---
+++ src/test/java/com/api/dialogflowapicommunication/DialogflowApiCommunicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.dialogflowapicommunication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DialogflowApiCommunicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
